1d3c5db8aa184133cfbaea1eae2ba541d0a70128
branden coker <branden.coker@gmail.com>
2019-08-07 13:26:08 -0400

    updated library from class

--- src/app/projects/projects-container/projects-container.component.ts
+++ src/app/projects/projects-container/projects-container.component.ts
@@ -1,21 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { MOCK_PROJECTS } from '../shared/mock-projects';
 import { Project } from '../shared/project.model';
-
+import { ProjectService } from '../shared/project.service';
 @Component({
   selector: 'app-projects-container',
   templateUrl: './projects-container.component.html',
   styleUrls: ['./projects-container.component.css']
 })
 export class ProjectsContainerComponent implements OnInit {
-  projects: Project[] = MOCK_PROJECTS;
-  constructor() {}
+  projects: Project[];
+  errorMessage: string;
+  loading: boolean;
 
-  ngOnInit() {}
+  constructor(private projectService: ProjectService) {}
+
+  ngOnInit() {
+    this.loading = true;
+    this.projectService.list().subscribe(
+      data => {
+        this.loading = false;
+        this.projects = data;
+      },
+      error => {
+        this.loading = false;
+        this.errorMessage = error;
+      }
+    );
+  }
 
   onSaveListItem(event: any) {
     const project: Project = event.item;
     const index = this.projects.findIndex(element => element.id === project.id);
     this.projects[index] = project;
+
+    this.projectService.put(project).subscribe(
+      updatedProject => {
+        const index = this.projects.findIndex(
+          element => element.id === project.id
+        );
+        this.projects[index] = updatedProject;
+      },
+      error => (this.errorMessage = error)
+    );
   }
 }
---
+++ src/app/projects/shared/project.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { Observable, of, throwError } from 'rxjs';
+import { catchError, delay } from 'rxjs/operators';
+import { Project } from './project.model';
+import { MOCK_PROJECTS } from './mock-projects';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders
+} from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectService {
+  private projectsUrl = environment.backendurl + '/projects/';
+
+  constructor(private http: HttpClient) {}
+
+  put(project: Project): Observable<Project> {
+    const url = this.projectsUrl + project.id;
+    return this.http.put<Project>(url, project, httpOptions).pipe(
+      catchError((error: HttpErrorResponse) => {
+        console.log(error);
+        return throwError('an error occurred updating the projects.');
+      })
+    );
+  }
+
+  list(): Observable<Project[]> {
+    return this.http.get<Project[]>(this.projectsUrl).pipe(
+      catchError((error: HttpErrorResponse) => {
+        console.log(error);
+        return throwError('an error occurred loading the projects.');
+      })
+    );
+  }
+}
